91654c9cfa0327846843a85716c0739a7c244f11
Kevin Bru <kevinbru@Kevins-MacBook-Pro.local>
2025-05-08 20:37:49 +0200

    change collection user -> try to modify the collection user auth / required fields

--- src/extensions/users-permissions/controllers/auth.js
+++ src/extensions/users-permissions/controllers/auth.js
@@ -1,35 +1,22 @@
-'use strict';
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('plugin::users-permissions.auth', ({ strapi }) => ({
     async register(ctx) {
-        const { body } = ctx.request;
+        const response = await super.register(ctx);
+        const { user } = response;
 
-        // Ensure required fields are present
-        if (!body.username || !body.email || !body.password) {
-            return ctx.badRequest('Missing required fields');
-        }
-
-        // Call the default register logic
-        const defaultRegister = await strapi
-            .plugin('users-permissions')
-            .controllers.auth.register(ctx);
-
-        // After registration, update extra fields
-        const userId = defaultRegister.user.id;
-
-        await strapi.entityService.update('plugin::users-permissions.user', userId, {
+        // Update additional user fields right after registration
+        await strapi.entityService.update('plugin::users-permissions.user', user.id, {
             data: {
-                last_name: body.last_name,
-                first_name: body.first_name,
-                phone: body.phone,
-                street: body.street,
-                postal_code: body.postal_code,
-                city: body.city,
-            },
+                last_name: ctx.request.body.last_name,
+                first_name: ctx.request.body.first_name,
+                phone: ctx.request.body.phone,
+                street: ctx.request.body.street,
+                postal_code: ctx.request.body.postal_code,
+                city: ctx.request.body.city,
+            }
         });
 
-        return defaultRegister;
-    },
+        return response;
+    }
 }));
