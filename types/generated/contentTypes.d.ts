604a3c6d38722b0cfa6c8e052c89c4a20fb2d9d1
Kevin Bru <kevinbru@Kevins-MacBook-Pro.local>
2025-05-14 10:31:45 +0200

    add collection continent + add relation between collection country & continent

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -369,6 +369,36 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiContinentContinent extends Struct.CollectionTypeSchema {
+  collectionName: 'continents';
+  info: {
+    displayName: 'Continent';
+    pluralName: 'continents';
+    singularName: 'continent';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    code: Schema.Attribute.String;
+    countries: Schema.Attribute.Relation<'oneToMany', 'api::country.country'>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::continent.continent'
+    > &
+      Schema.Attribute.Private;
+    name: Schema.Attribute.String;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiCountryCountry extends Struct.CollectionTypeSchema {
   collectionName: 'countries';
   info: {
@@ -383,6 +413,10 @@ export interface ApiCountryCountry extends Struct.CollectionTypeSchema {
   attributes: {
     code2: Schema.Attribute.String & Schema.Attribute.Unique;
     code3: Schema.Attribute.String & Schema.Attribute.Unique;
+    continent: Schema.Attribute.Relation<
+      'manyToOne',
+      'api::continent.continent'
+    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -962,6 +996,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::continent.continent': ApiContinentContinent;
       'api::country.country': ApiCountryCountry;
       'api::product.product': ApiProductProduct;
       'plugin::content-releases.release': PluginContentReleasesRelease;
