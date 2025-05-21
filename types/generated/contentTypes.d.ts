cef9c03367e6dbe7a0e9baadfe2048a9a84d14c7
Kevin Bru <kevinbru@Kevins-MBP.localdomain>
2025-05-21 11:00:59 +0200

    chanmge relation esim/user collection

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -504,6 +504,10 @@ export interface ApiEsimEsim extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    users_permissions_user: Schema.Attribute.Relation<
+      'oneToOne',
+      'plugin::users-permissions.user'
+    >;
   };
 }
 
@@ -1047,6 +1051,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    esim: Schema.Attribute.Relation<'oneToOne', 'api::esim.esim'>;
     esims: Schema.Attribute.Relation<'oneToMany', 'api::esim.esim'>;
     first_name: Schema.Attribute.String;
     has_billing_address: Schema.Attribute.Boolean;
