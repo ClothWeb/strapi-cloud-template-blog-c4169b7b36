dfce22b5deac90c835eab5a5cb1883fe4e71df57
Kevin Bru <kevinbru@Kevins-MBP.localdomain>
2025-05-21 16:58:30 +0200

    WIP

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -496,7 +496,7 @@ export interface ApiEsimEsim extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    user: Schema.Attribute.Relation<
+    users_permissions_user: Schema.Attribute.Relation<
       'manyToOne',
       'plugin::users-permissions.user'
     >;
