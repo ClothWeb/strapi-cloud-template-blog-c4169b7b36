e639ee91f76242785433a669fed37bbd1b4c668d
Kevin Bru <kevinbru@Kevins-MacBook-Pro.local>
2025-06-13 16:10:45 +0200

    test

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,25 @@
+// /src/extensions/users-permissions/strapi-server.js
+
+module.exports = (plugin) => {
+    plugin.controllers.user.me = async (ctx) => {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized();
+        }
+
+        const fullUser = await strapi.entityService.findOne(
+            'plugin::users-permissions.user',
+            user.id,
+            {
+                populate: ['country_test', 'billing_country'], // add anything you want to fetch
+            }
+        );
+
+        const sanitizedUser = await strapi.utils.sanitize.contentAPI.output(fullUser, strapi.getModel('plugin::users-permissions.user'));
+
+        ctx.body = sanitizedUser;
+    };
+
+    return plugin;
+};
